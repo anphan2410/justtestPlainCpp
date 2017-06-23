e6f4ba99fd28f17b075fe57df47f73b2aec395f7
anphan2410 <an.phan@ascenx.com>
2017-06-23 05:25:31 -0400

    add a.out

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    anMsg("=> This Should Be Normally Printed !", anForegroundGreen);
-    return 0;
+    	anMsg("=> This Should Be Normally Printed !", anForegroundGreen);
+    	std::cout << "This Line Should Be Shown In White !" << std::endl;
+	return 0;
 }
