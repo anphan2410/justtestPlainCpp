b07068fac9ef7aeb1cbe2f72a81b38f87def7408
anphan2410 <an.phan@ascenx.com>
2017-06-26 08:10:16 +0700

    test Linux Plain

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "anlogger.h"
 
+#define anMsg(msg, txtAttrib) {\
+        anTxtAttribType previousTxtAttrib = 0;\
+        if (!(_anGetConsoleTextAttribute(previousTxtAttrib)))\
+            previousTxtAttrib = anOriginalConsoleTextAttribute;\
+    }
+
 int main(int argc, char *argv[])
 
     anMsg("this is a test!", anForegroundGreen);
