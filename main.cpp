cb505e8ff19204f9a5dbb0109e24a65acf5678ef
anphan2410 <an.phan@ascenx.com>
2017-06-26 08:01:52 +0700

    test Linux Plain

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "anlogger.h"
 
+#define anMsgTest(msg, txtAttrib) { \
+		std::cout << "TEST GATE 1" << std::endl; \
+        anTxtAttribType previousTxtAttrib = 0; \
+        if (!(_anGetConsoleTextAttribute(previousTxtAttrib))) \
+            previousTxtAttrib = anOriginalConsoleTextAttribute; \
+		std::cout << "TEST GATE 2" << std::endl; \	
+        anSetConsoleTextAttribute(txtAttrib) \
+		std::cout << "TEST GATE 3" << std::endl; \
+        anMsgInputToMsgString(msg, tmp) \
+		std::cout << "TEST GATE 4" << std::endl; \
+        std::cerr << tmp; \
+		std::cout << "TEST GATE 5" << std::endl; \
+        anSetConsoleTextAttribute(previousTxtAttrib) \
 
 int main(int argc, char *argv[])
 
-    anMsg("this is a test!", anForegroundGreen);
+    anMsgTest("this is a test!", anForegroundGreen);
 
     return 0;
 }
