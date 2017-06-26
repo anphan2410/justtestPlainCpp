81bd2b8fb95816b1baf0ca49e522d9df2447c42b
anphan2410 <an.phan@ascenx.com>
2017-06-26 08:14:42 +0700

    test Linux Plain

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include "anlogger.h"
 
-#define anMsg(msg, txtAttrib) \
-        std::cout << "This Should Be Printed !" << std::endl;
-
 int main(int argc, char *argv[])
+{
 
     anMsg("this is a test!", anForegroundGreen);
 
