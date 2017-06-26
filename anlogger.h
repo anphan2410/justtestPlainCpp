05babf41c453f02dfc349f45aae3897176bea970
anphan2410 <an.phan@ascenx.com>
2017-06-26 07:12:06 +0700

    test on Linux Plain

--- anlogger.h
+++ anlogger.h
@@ -1,10 +1,18 @@
 #ifndef ANLOGGER_H
 #define ANLOGGER_H
-/*************Control Flags******************************************************/
+//PREREQUISITE: -std=c++11
+/************* Control Flags ****************************************************/
 //_anLoggerEnabled Is Zero => Logger Is Globally Disabled
 #define _anLoggerEnabled 1
-/*************Performance Flags**************************************************/
-#define _anExplicitTextAttributeRestoration 0
+//_anLoggerEnabled Is Zero => Logger Message Is Not Verbosely Positioned
+#define _anPositionEnabled 1
+/************* Performance Flags ************************************************/
+#define _anMessagePathTextAttribute anDefaultTextAttribute
+#define _anThreadIdPositionEnabled 1
+#define _anFunctionPositionEnabled 1
+#define _anFilePositionEnabled 1
+#define _anLinePositionEnabled 1
+#define _anTimePositionEnabled 1
 /********************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +20,8 @@
 #include <iostream>
 #include <sstream>
 #include <thread>
+#include <chrono>
+#include <time.h>
 #if defined _WINDOWS_ || defined __WINDOWS__ \
         || defined _WIN16 || defined __TOS_WIN__ \
         || defined _WIN32_WCE || defined _WIN32_WCE \
@@ -35,50 +45,20 @@
     #include <QIODevice>
     #define __anQt__
 #endif
-/*************Standalone Macros**************************************************/
+/************* Support Variables, Functions and Macros **************************/
 
-std::string anCurrentLocation() {
-    std::stringstream tmp;
-    tmp << std::this_thread::get_id();
-    return tmp.str();
-}
+static char anStdErrBuffer[BUFSIZ];
 
-#define __anPath__
+static const std::chrono::steady_clock::time_point anThisProgramStartingTimePoint = [](){
+    setvbuf(stderr, anStdErrBuffer, _IOFBF, BUFSIZ);
+    return std::chrono::steady_clock::now();
+}();
+#define __anStartTimePoint__ anThisProgramStartingTimePoint
+
+#define anTxtAttribType unsigned short
+
 #ifdef __anWINOS__
-    HANDLE anLastRetrievedStdOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO anLastRetrievedConsoleScreenBufferInfo;
-    WINBOOL anLastConsoleScreenBufferInfoProcurementBooleanResult =
-                GetConsoleScreenBufferInfo(anLastRetrievedStdOutputHandle,
-                                           &anLastRetrievedConsoleScreenBufferInfo);
-    WORD anLastRetrievedConsoleTextAttribute =
-            anLastRetrievedConsoleScreenBufferInfo.wAttributes;
-    WORD anGetCurrentConsoleTextAttribute() {
-        anLastRetrievedStdOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-        anLastConsoleScreenBufferInfoProcurementBooleanResult =
-                GetConsoleScreenBufferInfo(anLastRetrievedStdOutputHandle,
-                                           &anLastRetrievedConsoleScreenBufferInfo);
-        if(anLastConsoleScreenBufferInfoProcurementBooleanResult)
-        {
-            anLastRetrievedConsoleTextAttribute =
-                    anLastRetrievedConsoleScreenBufferInfo.wAttributes;
-            return anLastRetrievedConsoleTextAttribute;
-        }
-        else
-            return 0;
-    }
-    void anSetCurrentConsoleTextAttribute(WORD TextAttribute) {
-#if _anExplicitTextAttributeRestoration
-        anGetCurrentConsoleTextAttribute();
-#endif
-        if (TextAttribute)
-            anSetConsoleTextAttribute(anLastRetrievedStdOutputHandle,
-                                    TextAttribute);
-    }
-    void anRestorePreviousConsoleTextAttribute() {
-        if (anLastRetrievedConsoleTextAttribute)
-            anSetConsoleTextAttribute(anLastRetrievedStdOutputHandle,
-                                    anLastRetrievedConsoleTextAttribute);
-    }
+
 /*  8Bit - ConsoleTextAttribute
     bit 0 - foreground blue
     bit 1 - foreground green
@@ -88,6 +68,7 @@ std::string anCurrentLocation() {
     bit 5 - background green
     bit 6 - background red
     bit 7 - background intensity */
+    #define anForegroundBlack 0
     #define anForegroundRed 0b00001100
     #define anForegroundGreen 0b00001010
     #define anForegroundYellow 0b00001110
@@ -95,13 +76,28 @@ std::string anCurrentLocation() {
     #define anForegroundMagenta 0b00001101
     #define anForegroundCyan 0b00001011
     #define anForegroundWhite 0b00001111
+    #define anDefaultTextAttribute 0b00000111
 
-    #define anSetConsoleTextAttribute(TxtAttrib) \
-        anSetCurrentConsoleTextAttribute(TxtAttrib);
-    #define anRestoreConsoleTextAttribute \
-        anRestorePreviousConsoleTextAttribute();
+    static bool anGetCurrentConsoleTextAttribute(anTxtAttribType &OutputVar) {
+        CONSOLE_SCREEN_BUFFER_INFO tmp;
+        if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &tmp))
+        {
+            OutputVar = tmp.wAttributes;
+            return true;
+        }
+        return false;
+    }
+    #define _anGetConsoleTextAttribute(destination) \
+        anGetCurrentConsoleTextAttribute(destination)
+
+    #define anSetConsoleTextAttribute(TextAtrribute) \
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), TextAtrribute)
+
+    #define __anFilePathSlashChar__ u'\\'
 
 #elif defined __anLINUXOS__
+
+    #define anForegroundBlack 30
     #define anForegroundRed 31
     #define anForegroundGreen 32
     #define anForegroundYellow 33
@@ -109,38 +105,228 @@ std::string anCurrentLocation() {
     #define anForegroundMagenta 35
     #define anForegroundCyan 36
     #define anForegroundWhite 37
+    #define anDefaultTextAttribute 0
+
+    static bool anGetCurrentConsoleTextAttribute(anTxtAttribType &OutputVar) {
+        std::string tmpBuff = std::string(anStdErrBuffer);
+        anTxtAttribType tmp = tmpBuff.find_last_of(u8"\033[") + 2;
+        if (tmp != std::string::npos)
+        {
+            if ((tmpBuff.at(tmp) == u'0') && (tmpBuff.at(tmp+1) == u'm'))
+                OutputVar = 0;
+            else if ((tmpBuff.at(tmp) == u'3') && (tmpBuff.at(tmp+2) == u'm'))
+                OutputVar = std::stoi(tmpBuff.substr(m,2));
+            else
+                return false;
+            return true;
+        }
+        else
+            return false;
+    }
+    #define _anGetConsoleTextAttribute(destination) \
+        anGetCurrentConsoleTextAttribute(destination)
 
     #define anSetConsoleTextAttribute(TxtAttrib) \
-        std::cerr << "\033[" \
+        std::cerr << u8"\033[" \
         << std::to_string(TxtAttrib)\
-        << "m";
-    #define anRestoreConsoleTextAttribute \
-        std::cerr << "\033[0m";
+        << u8"m";
+
+    #define __anFilePathSlashChar__ u'/'
+
 #endif
+
+static const anTxtAttribType anOriginalConsoleTextAttribute = [](){
+    anTxtAttribType tmp = anDefaultTextAttribute;
+    anGetCurrentConsoleTextAttribute(tmp);
+    return tmp;
+}();
+#define __anOriginalConsoleTextAttribute__ anOriginalConsoleTextAttribute
+
+#if _anPositionEnabled && (_anThreadIdPositionEnabled \
+        || _anFunctionPositionEnabled || _anFilePositionEnabled \
+        || _anLinePositionEnabled || _anTimePositionEnabled)
+
+    #if _anFilePositionEnabled
+
+        static const std::string anGetCurrentFileName(const std::string &currentFilePath) {
+            return currentFilePath.substr(
+                        currentFilePath.find_last_of(__anFilePathSlashChar__)+1);
+        }
+        #define __anFILENAME__ anGetCurrentFileName(__FILE__)
+
+    #endif
+
+    #if _anTimePositionEnabled
+
+        #define __anElapsedTimeNSEC__ \
+            std::chrono::duration_cast<std::chrono::nanoseconds>( \
+                std::chrono::steady_clock::now() - __anStartTimePoint__).count()
+    #endif
+
+    static const std::string anCurrentMessagePath(
+            #if _anTimePositionEnabled
+                const unsigned long int &currentElapsedTime,
+            #endif
+            #if _anThreadIdPositionEnabled
+                const std::thread::id &currentThreadId,
+            #endif
+            #if _anFunctionPositionEnabled
+                const char* currentFunctionName,
+            #endif
+            #if _anFilePositionEnabled
+                const std::string &currentFileName,
+            #endif
+            #if _anLinePositionEnabled
+                const unsigned int &currentLine
+            #endif
+            ) {
+        std::stringstream tmp;
+        tmp
+            #if _anThreadIdPositionEnabled
+                << u8"|" << currentThreadId
+            #endif
+            #if _anFunctionPositionEnabled
+                << u8"|" << currentFunctionName
+            #endif
+            #if _anFilePositionEnabled
+                << u8"|" << currentFileName
+            #endif
+            #if _anLinePositionEnabled
+                << u8"|" << currentLine
+            #endif
+            #if _anTimePositionEnabled
+                << u8"|" << currentElapsedTime
+            #endif
+            << u8"|";
+        return tmp.str();
+    }
+
+    #if _anTimePositionEnabled
+        #define anTmpTimeParam __anElapsedTimeNSEC__,
+    #else
+        #define anTmpTimeParam
+    #endif
+
+    #if _anThreadIdPositionEnabled
+        #define anTmpThreadIdParam std::this_thread::get_id(),
+    #else
+        #define anTmpThreadIdParam
+    #endif
+
+    #if _anFunctionPositionEnabled
+        #define anTmpFunctionParam __PRETTY_FUNCTION__,
+    #else
+        #define anTmpFunctionParam
+    #endif
+
+    #if _anFilePositionEnabled
+        #define anTmpFileNameParam __anFILENAME__,
+    #else
+        #define anTmpFileNameParam
+    #endif
+
+    #if _anLinePositionEnabled
+        #define anTmpLineParam __LINE__
+    #else
+        #define anTmpLineParam
+    #endif
+
+    #define __anMessagePath__ \
+        anCurrentMessagePath(anTmpTimeParam \
+                             anTmpThreadIdParam \
+                             anTmpFunctionParam \
+                             anTmpFileNameParam \
+                             anTmpLineParam)
+
+#else
+    #define _anPositionDisabled
+#endif
+
 /********************************************************************************/
 #if _anLoggerEnabled
 /********************************************************************************/
     #define anLogCode(...) __VA_ARGS__
+
 #ifdef __anQt__
-    #define anMsgWithColor(msg, TxtAttrib) \
-        anSetConsoleTextAttribute(TxtAttrib) \
-        QTextStream(stderr, QIODevice::WriteOnly) << msg; \
-        anRestoreConsoleTextAttribute
-    #define anMsg(msg) QTextStream(stderr, QIODevice::WriteOnly) << msg;
+    #define anMsgInputToMsgString(msgIn,msgStr) \
+            std::string msgStr; \
+            { \
+                QString * tmpQStrrBefore##msgStr = new QString(u8""); \
+                QTextStream(tmpQStrrBefore##msgStr, QIODevice::ReadWrite) << msgIn; \
+                msgStr = tmpQStrrBefore##msgStr->toStdString(); \
+                delete tmpQStrrBefore##msgStr; \
+                tmpQStrrBefore##msgStr = Q_NULLPTR; \
+            }
 #else
-    #define anMsgWithColor(msg, TxtAttrib) \
-        anSetConsoleTextAttribute(TxtAttrib) \
-        std::cerr << msg; \
-        anRestoreConsoleTextAttribute
-    #define anMsg(msg) std::cerr << msg;
+    #define anMsgInputToMsgString(msgIn,msgStr) \
+            std::string msgStr; \
+            { \
+                std::stringstream tmpStrStrmBefore##msgStr; \
+                tmpStrStrmBefore##msgStr << msgIn; \
+                msgStr = tmpStrStrmBefore##msgStr.str(); \
+            }
 #endif
+
+#ifndef _anPositionDisabled
+    static void anNoLineMessageLogger(const std::string &aNoLineMessage,
+                                  const std::string &msgPath,
+                                  const anTxtAttribType &preTxtAttrib) {
+        std::cerr << aNoLineMessage;
+        anSetConsoleTextAttribute(_anMessagePathTextAttribute);
+        std::cerr << msgPath;
+        anSetConsoleTextAttribute(preTxtAttrib);
+    }
+    static void anColorizedMessageLogger(std::string &rawMsgStr,
+                                         const anTxtAttribType &txtAttrib,
+                                         const std::string &currentMsgPath,
+                                         const anTxtAttribType &previousTxtAttrib) {
+        anSetConsoleTextAttribute(txtAttrib);
+        std::string nowPath = u8"<-" + currentMsgPath;
+        std::string tmpPath = nowPath + u8"\n";
+        for(std::string::size_type i = 0;(i = rawMsgStr.find(u8"\n", 0)) != std::string::npos;)
+        {
+            anNoLineMessageLogger(rawMsgStr.substr(0,i++),tmpPath,txtAttrib);
+            rawMsgStr = rawMsgStr.substr(i);
+        }
+        if (!rawMsgStr.empty())
+            anNoLineMessageLogger(rawMsgStr,nowPath,txtAttrib);
+        anSetConsoleTextAttribute(previousTxtAttrib);
+    }
+
+    #define anMsg(msg, aTextAttribute) { \
+        std::string currentMessagePath = __anMessagePath__; \
+        anTxtAttribType prevTxtAttrib = 0; \
+        if (!(_anGetConsoleTextAttribute(prevTxtAttrib))) \
+            prevTxtAttrib = anOriginalConsoleTextAttribute; \
+        anMsgInputToMsgString(msg, tmpMsgStr) \
+        anColorizedMessageLogger(tmpMsgStr, aTextAttribute, currentMessagePath, prevTxtAttrib); \
+    }
+#else
+
+    #define anMsg(msg, txtAttrib) { \
+        anTxtAttribType previousTxtAttrib = 0; \
+        if (!(_anGetConsoleTextAttribute(previousTxtAttrib))) \
+            previousTxtAttrib = anOriginalConsoleTextAttribute; \
+        anSetConsoleTextAttribute(txtAttrib); \
+        anMsgInputToMsgString(msg, tmp) \
+        std::cerr << tmp; \
+        anSetConsoleTextAttribute(previousTxtAttrib); \
+    {
+#endif
+
+#define anDbg(msg, condition) if (condition) \
+                                anMsg(u8"=> " << msg << "\n", anForegroundCyan)
+#define anInfo(msg) anMsg(u8"   " << msg << "\n", anForegroundWhite)
+#define anAck(msg) anMsg(u8"=> " << msg << "\n", anForegroundGreen)
+#define anWarning(msg) anMsg(u8"=> " << msg << "\n", anForegroundYellow)
+#define anError(msg) anMsg(u8"=> " << msg << "\n", anForegroundRed)
+
 /********************************************************************************/
 
 
 /********************************************************************************/
 #else
     #define anLogCode(...)
-    #define anMsgWithColor(msg, TxtAttrib)
     #define anMsg(msg)
 #endif
 #endif // ANLOGGER_H
--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include "anlogger.h"
-using namespace std;
+
 
 int main(int argc, char *argv[])
-{
-        anMsgWithColor("=> This Should Be Normally Printed !" << std::endl, anForegroundGreen);
-        cout << __PRETTY_FUNCTION__ << endl;
-#ifdef _WIN32
-        std::string tmp(__FILE__);
-        cout << tmp.substr(tmp.find_last_of("/\\")) << endl;
-#else
 
-#endif
-        std::cout << std::endl << "This Line Should Be Shown In White !" << std::endl;
-	return 0;
+    anSetConsoleTextAttribute(anForegroundMagenta);
+
+    anAck("Success !");
+    anInfo("It is running !");
+    anWarning("too disappointed !");
+    anError("oh no it failed !");
+
+    std::cout << "END PROGRAM !" << std::endl;
+
+    return 0;
 }
